2b7d1390ca9da7dfabbdc67f4412f4832f77cef8
omarhussein244 <omarhussein244@yahoo.com>
2020-07-05 12:19:55 +0200

    Logging system commit

--- include/Logger.h
+++ include/Logger.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <Core.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
 namespace Imp {
 	class EXPORT Logger {
 	public:
+		void init();
 		Logger();
 		~Logger();
+		//inline static std::shared_ptr<spdlog::logger>& coreLog() { return coreLogger; }
+		//inline static std::shared_ptr<spdlog::logger>& clientLog() { return clientLogger; }
+	private:
+		 std::shared_ptr<spdlog::logger> coreLogger;
+		 std::shared_ptr<spdlog::logger> clientLogger;
 	};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Kaizen.h>
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <Logger.h>
+
 
 int main() {
 	auto console = spdlog::stdout_color_mt("console");
+	//Imp::Logger::init();
 	std::unique_ptr<Imp::Kaizen> kai = std::make_unique<Imp::Kaizen>();
+	std::unique_ptr<Imp::Logger> log = std::make_unique<Imp::Logger>();
+	log->init();
 	kai->run();
 	return 0;
 }
